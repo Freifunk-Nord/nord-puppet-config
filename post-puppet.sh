53ebb3f02a2e94132902e3842b8961584ba63a6d
rubo77 <github@r.z11.de>
2016-11-15 14:41:25 +0100

    radvd angepasst, damit das default gateway nicht mehr announced wird

--- post-puppet.sh
+++ post-puppet.sh
@@ -3,6 +3,7 @@
 
 VPN_NUMBER=0
 DOMAIN=nord.freifunk.net
+COMMUNITY_TLD=ffnord
 
 #NGINX, if needed to serve the firmware for the auto-updater
 #apt-get install -y nginx
@@ -24,6 +25,34 @@ nameserver 62.141.32.3
 nameserver 8.8.8.8
 
 EOF
+
+mv /etc/radvd.conf /etc/radvd.conf.bak
+cat >> /etc/radvd.conf << EOF
+# managed for interface br-$COMMUNITY_TLD
+interface br-$COMMUNITY_TLD
+{
+ AdvSendAdvert on;
+ AdvDefaultLifetime 0; # Here
+ IgnoreIfMissing on;
+ MaxRtrAdvInterval 200;
+
+ prefix fda1:384a:74de:4242:0000:0000:0000:0000/64
+ {
+   AdvPreferredLifetime 14400; # Here
+   AdvValidLifetime 86400; # Here
+ };
+
+ RDNSS fda1:384a:74de:4242::ff0$VPN_NUMBER
+ {
+ };
+
+ route fc00::/7  # this block
+ {
+   AdvRouteLifetime 1200;
+ };
+};
+EOF
+cp /etc/radvd.conf /etc/radvd.conf.d/interface-br-ffki.conf
 
 # check if everything is running:
 service fastd restart
