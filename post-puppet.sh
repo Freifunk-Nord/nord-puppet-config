ce4b02a6d30d58995ec04b9e8bd570eeb3fe61f9
rubo77 <github@r.z11.de>
2016-11-15 15:44:42 +0100

    add dns-search directly after the line "eth0 inet static"

--- post-puppet.sh
+++ post-puppet.sh
@@ -12,7 +12,7 @@ TLD=ffnord
 #sed s~"usr/share/nginx/www;"~"opt/www;"~g -i /etc/nginx/sites-enabled/default
 
 #DNS Server
-echo "dns-search vpn$VPN_NUMBER.$DOMAIN" >>/etc/network/interfaces
+sed -i .bak "/eth0 inet static/a \  dns-search vpn$VPN_NUMBER.$DOMAIN" >>/etc/network/interfaces
 
 rm /etc/resolv.conf
 cat >> /etc/resolv.conf <<-EOF
