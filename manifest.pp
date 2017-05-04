30d87ebbe26f1fd2976acc421d35c6d7ec39c712
rubo77 <github@r.z11.de>
2017-05-04 19:31:24 +0200

    set batman_version to 15

--- manifest.pp
+++ manifest.pp
@@ -15,6 +15,8 @@ class { 'ffnord::params':
   include_bird4 => false,
   maintenance => 0,
   #debian_mirror => "http://repo.myloc.de/mirrors/ftp.de.debian.org/debian/";
+  
+  $batman_version => 15,            # B.A.T.M.A.N. adv version
 }
 # aus https://github.com/ffnord/site-nord/blob/master/site.conf
 # und https://github.com/freifunk/icvpn-meta/blob/master/nord
